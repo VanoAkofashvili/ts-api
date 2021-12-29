9197513fb82b2a0e0c180e319e2a7daaf9b464c3
Vaniko Akopashvili <v.akopashvili@sweeftdigital.com>
2021-12-29 22:02:28 +0400

    Seperate db variables in .env

--- src/index.ts
+++ src/index.ts
@@ -2,26 +2,23 @@ import { app } from './app';
 import pool from './pool';
 
 const start = async () => {
-  if (!process.env.JWT_KEY) {
-    throw new Error('JWT_KEY must be defined');
+  if (!process.env.DB_NAME) {
+    throw new Error('DB_NAME must be defined')
   }
-  if (!process.env.DATABASE_NAME) {
-    throw new Error('DATABASE_NAME must be defined')
+  if (!process.env.DB_USER) {
+    throw new Error('DB_USER must be defined');
   }
-  if (!process.env.DATABASE_USER) {
-    throw new Error('DATABASE_USER must be defined');
-  }
-  if (!process.env.DATABASE_PASSWORD) {
-    throw new Error('DATABASE_PASSWORD must be defined');
+  if (!process.env.DB_PASSWORD) {
+    throw new Error('DB_PASSWORD must be defined');
   }
 
   try {
     await pool.connect({
       host: 'localhost',
       port: 5432,
-      database: process.env.DATABASE_NAME,
-      user: process.env.DATABASE_USER,
-      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DB_NAME,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
     });
   } catch (err) {
     console.error(err);
