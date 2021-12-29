499b56535891480d8189312579fa1282446760cb
Vaniko Akopashvili <v.akopashvili@sweeftdigital.com>
2021-12-29 21:54:07 +0400

    Starting point

--- src/pool.ts
+++ src/pool.ts
@@ -1,11 +1,23 @@
-import pg from "pg";
+import pg, { Pool as PoolType, PoolConfig } from "pg";
 
 class Pool {
-  private pool = null;
+  private _pool: null | PoolType = null;
 
-  connect(options) {
-    this.pool = new pg.Pool()
+  connect(options: PoolConfig) {
+    this._pool = new pg.Pool(options);
+    return this._pool.query('SELECT 1+1');
   }
+
+  close() {
+    if (this._pool) {
+      return this._pool.end();
+    }
+  }
+
+  query(sql: string, params: unknown[]) {
+    return this._pool?.query(sql, params);
+  }
+
 }
 
 export default new Pool();
